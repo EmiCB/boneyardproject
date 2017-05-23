415649bbafdf0c9e538ff9e73cff9491a1335eef
EmiCB (Emi Brown) <emicb55@gmail.com>
2017-05-22 20:39:53 -0500

    Update 4.7.1 (bad)

---
+++ backups/boneyardproject-master/README.md
@@ -0,0 +1,2 @@
+# boneyardproject.github.io
+A website to show off the gallery of art for our freshman Boneyard Project
--- nav.js
+++ nav.js
@@ -1,17 +1,11 @@
-$(document).ready(function() {
-	//change the integers below to match the height of your upper div
-	//Just add a 1 to the last number.  console.log($(window).scrollTop()) to figure out what the scroll position is when exactly you want to fix the nav
-	//bar or div or whatever.  I stuck in the console.log for you.  Just remove when you know the position.
-	$(window).scroll(function () { 
+var  mn = $("main-nav");
+    mns = "main-nav-scrolled";
+    hdr = $('header').height();
 
-		console.log($(window).scrollTop());
-
-		if ($(window).scrollTop() > 550) {
-			$('#nav').addClass('navbar-fixed');
-		}
-
-		if ($(window).scrollTop() < 551) {
-			$('#nav').removeClass('navbar-fixed');
-		}
-	});
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
 });
